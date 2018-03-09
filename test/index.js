750893acf61ab3ef1647b25e624d94a0dc2070ff
Emanuele Falzone <emafalzo@gmail.com>
2018-03-09 11:05:15 +0100

    Add sample test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+/*jslint node: true, nomen: true*/
+"use strict";
+
+var assert = require('assert');
+
+describe('Test', function () {
+    it('should pass', function () {
+        assert.equal(true, true);
+    });
+});
