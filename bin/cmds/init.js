cb2fd66b2a81dced33db98e74d00aae6767cafc3
Emanuele Falzone <emafalzo@gmail.com>
2018-08-08 14:02:35 +0200

    Update init script

--- bin/cmds/init.js
+++ bin/cmds/init.js
@@ -4,7 +4,7 @@ exports.command = 'init'
 exports.desc = 'Initialize the repo to use almost-git'
 exports.builder = {}
 exports.handler = () => {
-  lib.init().then(() => {
+  lib.init('.').then(() => {
     console.log('Repository correctly initialized!')
   }).catch((err) => {
     console.error(err);
